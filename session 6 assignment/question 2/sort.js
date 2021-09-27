a17b2c0befc6cf8d6a45cff7190e8d9ddd07723c
cha1v <90302773+cha1v@users.noreply.github.com>
2021-09-27 22:20:44 +0530

    Add files via upload

--- session 6 assignment/question 2/sort.js
+++ session 6 assignment/question 2/sort.js
@@ -1,1 +1,17 @@
-
+const items = [ { "item": "Dal","type": "veg"},
+            {"item": "Fish", "type": "non-veg"},
+            {
+              "item": "Meat", "type": "non-veg" },
+            {
+              "item": "Rice","type": "veg" } ];
+          
+          
+              
+                  
+                      const res= items.reduce((val, { item,type }) => {
+            if (!val[type]) val[type] = [];
+           val[type].push(item);
+            return val;
+          }, {})
+          console.log(res);
+         
