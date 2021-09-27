da8a3e48592b7f4a8dbbe10581854e1dfa3b1eab
cha1v <90302773+cha1v@users.noreply.github.com>
2021-09-27 22:05:17 +0530

    Add files via upload

---
+++ session 6 assignment/question 3/src/App.js
@@ -0,0 +1,94 @@
+import "./App.css";
+
+import { useForm } from "react-hook-form";
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    trigger,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
+
+  
+
+  return (
+    <div className="container pt-5">
+      <div className="row justify-content-sm-center pt-5">
+        <div className="col-sm-6 shadow round pb-3">
+          <h1 className="text-center pt-3 text-secondary">User Input Form</h1>
+          <form onSubmit={ handleSubmit(onSubmit)}>
+            <div className="form-group">
+              <label className="col-form-label">Name:</label>
+              <input
+                type="text"
+                className={`form-control ${errors.name && "invalid"}`}
+                {...register("name", { required: "Name is Required" })}
+                onKeyUp={() => {
+                  trigger("name");
+                }}
+              />
+              {errors.name && (
+                <small className="text-danger">{errors.name.message}</small>
+              )}
+            </div>
+            
+            <div className="form-group">
+              <label className="col-form-label">Email:</label>
+              <input
+                type="text"
+                className={`form-control ${errors.email && "invalid"}`}
+                {...register("email", { required: "Email is Required" ,
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Invalid email address",
+                }})}
+                onKeyUp={() => {
+                  trigger("email");
+                }}
+              />
+              {errors.email && (
+                <small className="text-danger">{errors.email.message}</small>
+              )}
+            </div>
+            <div className="form-group">
+              <label className="col-form-label">Phone:</label>
+              <input
+                type="text"
+                className={`form-control ${errors.phone && "invalid"}`}
+                {...register("phone", { required: "Phone is Required",
+                pattern: {
+                  value: /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
+                  message: "Invalid phone no",
+                },
+               })}
+               onKeyUp={() => {
+                trigger("phone");
+              }}
+              />
+              {errors.phone && (
+                <small className="text-danger">{errors.phone.message}</small>
+              )}
+            </div>
+            
+              
+            <input
+              type="submit"
+              className="btn btn-primary my-3"
+              value="Submit"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
