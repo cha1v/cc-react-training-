ace0e3514ba50a99c8fd99d50ca39b9128fe5615
cha1v <90302773+cha1v@users.noreply.github.com>
2021-10-01 19:20:09 +0530

    Add files via upload

---
+++ session 8/fourth question/src/App.js
@@ -0,0 +1,20 @@
+import Sidebar from './components/Sidebar'
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+  <div className="App" >
+  <Header/>
+  <Sidebar/>
+    <Footer/>
+
+    
+   </div>
+  );
+
+      }
+
+export default App
---
+++ session 8/fourth question/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../App.css';
+function Footer() {
+ return( <div className="footer">
+    <p>@react.js</p>
+    
+  </div>
+ );
+}
+
+export default Footer
