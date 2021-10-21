bb2e6d2c0230a7c2f2381266c98ca21a22f7e6a4
cha1v <90302773+cha1v@users.noreply.github.com>
2021-10-21 22:52:50 +0530

    Delete Session 12 Assignment/question 1 directory

--- Session 12 Assignment/question 1/src/components/Searchbar.js
+++
@@ -1,8 +0,0 @@
-const Searchbar =({onSearch}) => (
-    <div>
-        <label htmlFor="search">Search for :</label> 
-        <input id="search" type="text" onChange={onSearch}/>
-        
-       </div>
-)
-export default Searchbar
